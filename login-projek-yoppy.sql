86ead7641db522bef1ebb48fd943495a9829b1e5
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2021-06-20 18:47:54 +0700

    update SQL

--- login-projek-yoppy.sql
+++ login-projek-yoppy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 20, 2021 at 01:12 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.8
+-- Host: localhost:3306
+-- Generation Time: Jun 20, 2021 at 01:47 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,18 +29,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `id` int(6) NOT NULL,
-  `nama` varchar(16) NOT NULL,
+  `nama` varchar(64) NOT NULL,
   `gender` tinyint(1) NOT NULL,
-  `tanggal_lahir` datetime NOT NULL,
-  `alamat` varchar(50) NOT NULL,
-  `kota` varchar(50) NOT NULL,
-  `provinsi` varchar(50) NOT NULL,
-  `telepon` varchar(14) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `tanggal_lahir` datetime(6) NOT NULL,
+  `alamat` varchar(64) NOT NULL,
+  `kota` varchar(64) NOT NULL,
+  `provinsi` varchar(64) NOT NULL,
+  `telepon` varchar(16) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `username` varchar(64) NOT NULL,
   `pass` varchar(16) NOT NULL,
-  `sandi` varchar(16) NOT NULL
+  `sandi` varchar(16) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `gender`, `tanggal_lahir`, `alamat`, `kota`, `provinsi`, `telepon`, `email`, `username`, `pass`, `sandi`) VALUES
+(1, 'Muhammad Wahid', 1, '2001-03-28 07:00:18.000000', 'Perum Tegal Besar Permai 1 EY2', 'Jember', 'Jawa Timur', '085230448992', 'wahid28@gmail.com', 'wahid28', 'pass', 'pass'),
+(2, 'Adinda Yoppy', 0, '2000-12-08 07:00:18.000000', 'Jalan Batu Raden XXIX', 'Jember', 'Jawa Timur', '082142275777', 'Yoppy123@gmail.com', 'dnxhill_', 'popeye', NULL);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +68,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
